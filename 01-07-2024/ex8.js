062d82da24f40fe74f25aad39a9f14d53d8f12a5
giovanni <gspinelli028@gmail.com>
2024-07-02 07:46:11 -0300

    pequenas modificações

--- 01-07-2024/ex8.js
+++ 01-07-2024/ex8.js
@@ -1,10 +1,3 @@
-/*
-- Utilize um for loop para gerar um novo array com apenas os números entre  
-- 11 e 90 presentes no array "numbers" (incluindo 11 e 90);
-- Exiba o novo array no console;
-- O resultado deve ser: [34, 46, 90, 25, 11, 89, 76].
-*/
-
 const numbers = [7, 92, 34, 46, 90, 25, 11, 3, 89, 76, 99];
 
 const newNumbers = [];
